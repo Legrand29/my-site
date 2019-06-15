8e2fbf5b792e91b2f171b6ed32810f28712ec756
Legrand29 <lake.frantz@gmail.com>
2019-06-15 17:47:45 -0400

    first commit

---
+++ js/bin/main.js
@@ -0,0 +1,23 @@
+// Navigation JS
+  // jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+//window.onscroll = function() {myFunction()};
+//
+//var navbar = document.getElementById("navbar");
+//var sticky = navbar.offsetTop;
+//
+//function myFunction() {
+//  if (window.pageYOffset >= sticky) {
+//    navbar.classList.add("sticky")
+//  } else {
+//    navbar.classList.remove("sticky");
+//  }
+//
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
